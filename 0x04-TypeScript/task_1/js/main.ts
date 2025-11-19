e222ec122b2b19f2f83b074bde4383724849157f
hewan <hewanzewdie16@gmail.com>
2025-11-19 10:28:04 -0800

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,52 @@
+//teacher
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  contract: boolean;
+  yearsOfExperience?: number;
+  [key: string]: any;
+}
+
+const teacher1: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: true,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher1);
+
+//director
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Director = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  location: 'New York',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+  contract: false,
+};
+
+console.log(director1);
+
+interface printTeacherFunction {
+  ({ firstName, lastName }: { firstName: string; lastName: string }): string;
+}
+
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
+  firstName = firstName[0]; 
+  return `${firstName}. ${lastName}`;
+}
+
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
+
+//student class
 interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
