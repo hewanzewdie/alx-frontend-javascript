33fdcb33558cce5f4054d91f99e6b2a73fe7de54
hewan <hewanzewdie16@gmail.com>
2025-11-19 10:11:41 -0800

    upate

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,49 +1,3 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  location: string;
-  contract: boolean;
-  yearsOfExperience?: number;
-  [key: string]: any;
-}
-
-const teacher1: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: true,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher1);
-
-interface Director extends Teacher {
-  numberOfReports: number;
-}
-
-const director1: Director = {
-  firstName: 'Jane',
-  lastName: 'Doe',
-  location: 'New York',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-  contract: false,
-};
-
-console.log(director1);
-
-interface printTeacherFunction {
-  ({ firstName, lastName }: { firstName: string; lastName: string }): string;
-}
-
-function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
-  firstName = firstName[0]; 
-  return `${firstName}. ${lastName}`;
-}
-
-console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
-
 interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
@@ -52,7 +6,6 @@ interface StudentClassInterface {
 interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
-
 class StudentClass implements StudentClassInterface {
   private firstName: string;
   private lastName: string;
