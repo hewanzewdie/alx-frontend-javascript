3108a29d41f3d7672e3d316ee530f07fec5c70a3
hewan <hewanzewdie16@gmail.com>
2025-11-19 10:59:51 -0800

    update

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,23 @@
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud.js";
+
+// Create a row object
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva"
+};
+
+// Insert the row
+const newRowID: RowID = CRUD.insertRow(row);
+
+// Update the row with age
+const updatedRow: RowElement = {
+  ...row,
+  age: 23
+};
+CRUD.updateRow(newRowID, updatedRow);
+
+// Delete the row
+CRUD.deleteRow(newRowID);
